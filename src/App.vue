515a85cb4c99532f19b2416cad692fa1e7861a3b
Dedal378 <orlovetsfoto@gmail.com>
2021-07-08 14:21:13 +0300

    add first view

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,29 @@
 <template>
-  <div id="app">
+  <div class="container" id="app">
+    <Header />
     <Main />
   </div>
 </template>
 
 <script>
+import Header from '@/components/Header'
 import Main from '@/components/Main'
 
 export default {
   name: 'App',
   components: {
-    Main
+    // eslint-disable-next-line vue/no-unused-components
+    Header, Main
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-
+.container {
+  display: grid;
+  grid-template-columns: 60px repeat(auto-fill, minmax(60px, 100vw));
+  gap: 20px;
+  height: 100vh;
+  overflow-y: hidden;
 }
 </style>
