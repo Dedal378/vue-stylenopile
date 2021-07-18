3e39a891f87d6720296079dc7d03628504c17422
Dedal378 <orlovetsfoto@gmail.com>
2021-07-19 01:00:55 +0300

    add loader in main

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
-  <div class="container" id="app">
-    <Header />
-    <Main />
+  <div class="">
+    <div class="loader2"></div>
+    <div class="loader"></div>
+    <div class="container" id="app">
+      <Header />
+      <Main />
+    </div>
   </div>
 </template>
 
@@ -12,7 +16,6 @@ import Main from '@/components/Main'
 export default {
   name: 'App',
   components: {
-    // eslint-disable-next-line vue/no-unused-components
     Header, Main
   }
 }
@@ -20,10 +23,57 @@ export default {
 
 <style lang="scss">
 .container {
+  position: relative;
   display: grid;
   grid-template-columns: 60px repeat(auto-fill, minmax(60px, 100vw));
   gap: 20px;
   height: 100vh;
   overflow-y: hidden;
+}
+
+.loader {
+  position: absolute;
+  width: 100vw;
+  height: 110vh;
+  background-color: #686868;
+  transform: translateY(100vh);
+  animation: loader-up-down 5s cubic-bezier(.39, .39, .32, 1.19);
+  opacity: 1;
+  z-index: 100;
+}
+
+.loader2 {
+  position: absolute;
+  width: 100vw;
+  height: 100vh;
+  background-color: #bdbdbd;
+  animation: loader-down 5s cubic-bezier(.39, .39, .32, 1.19);
+  animation-fill-mode: forwards;
+  opacity: 1;
+  z-index: 99;
+}
+
+@keyframes loader-up-down {
+  from {
+    transform: translateY(100vh);
+  }
+  50% {
+    transform: translateY(0);
+  }
+  to {
+    transform: translateY(100vh);
+  }
+}
+
+@keyframes loader-down {
+  from {
+    transform: translateY(0);
+  }
+  50% {
+    transform: translateY(0);
+  }
+  to {
+    transform: translateY(100vh);
+  }
 }
 </style>
